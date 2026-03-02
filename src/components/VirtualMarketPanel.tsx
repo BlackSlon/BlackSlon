3d7ba60f5b9f3bc39b724d9a649e71f04b4d228d
BlackSlon <trading@blackslon.org>
2026-03-02 10:08:56 +0000

    DARKER PURPLE: Change BlackSlon Power Poland from purple-500 to purple-700

--- src/components/VirtualMarketPanel.tsx
+++ src/components/VirtualMarketPanel.tsx
@@ -38,7 +38,7 @@ export default function VirtualDimension({ marketId }: { marketId: string }) {
       <div className="flex-grow px-6 pb-6 flex flex-col min-h-0">
         {/* 2. TYTUŁ INSTRUMENTU (Fioletowy) */}
         <div className="p-4 bg-gradient-to-b from-black to-gray-950">
-          <div className="text-[12px] tracking-widest text-purple-500 mb-1">
+          <div className="text-[12px] tracking-widest text-purple-700 mb-1">
             BlackSlon Power Poland
           </div>
         <div className="flex justify-between items-center mt-2">
